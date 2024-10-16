e51d4c565bea0d4cd924a559ae43fedab5388a7c
Andrei <andrei.mancijo1@gmail.com>
2024-10-16 18:26:53 -0300

    Insert structures of file management

--- Andrei/main.c
+++ Andrei/main.c
@@ -8,39 +8,63 @@ int main()
         int id;
         char name[30];
         float price;
-    };
-    typedef struct product Product;
+    };                              // Estrutura dos dados
+    typedef struct product Product; // Define Product
 
 
-    FILE *arqIn, *arqOut;
+    FILE *arqIn, *arqOut; // Ponteiros do arquivo
 
-    Product productIn;
+
+    Product productIn;  // Cria a variavel productIn com a estrutura Product
+    size_t fSize; // variavel com tipo próprio para tamanhos de arquivos
 
     arqOut = fopen("productList.dat", "ab");
     if(!arqOut){
         printf("file error");
         return(0);
-    }
+    }    // Abre o arquivo em forma de append e valida se abriu
     do {
-        fflush(stdin);
-        system("cls");
+        fflush(stdin);   // limpa o buffer do teclado
+        system("cls");   // limpa a tela
 
-        printf("Digite 0 para sair\nid: ");
-        scanf("%d", &productIn.id);
-        if(productIn.id == 0) break;
+        printf("Digite 0 para sair\nid: ");  // O programa encerra quando digitar 0 no inicio
+        scanf("%d", &productIn.id);     // Coleta o ID
+        if(productIn.id == 0) break;    // Se 0 for inserido, encerra
         fflush(stdin);
 
         printf("nome: ");
-        gets(productIn.name);
+        gets(productIn.name); // Coleta o nome
+        fflush(stdin);
 
         printf("preco: ");
-        scanf("%f", &productIn.price);
+        scanf("%f", &productIn.price);  // Coleta o preço
+        fflush(stdin);
 
-        fwrite(&productIn, sizeof(productIn), 1, arqOut);
-    } while(1);
+        fwrite(&productIn, sizeof(productIn), 1, arqOut);   // Escreve no arquivo
+    } while(1);     // Loop se encerra apenas com o break
 
-    fclose(arqOut);
+    fclose(arqOut);  // Fecha o Arquivo
 
-    siistem("cls");
-    printf()
+
+    arqIn = fopen("productList.dat", "rb");  // Abre o
+    if(!arqIn){
+        printf("file error");
+        return(0);
+    }   // Abre o arquivo como leitura e verifica se abriu
+
+    system("cls");      // Limpa a tela
+
+    fseek(arqIn, 0l, SEEK_END); //  Define o ponteiro ao final do arquivo
+    fSize = ftell(arqIn); // Função retorna a posição do ponteiro, descobrindo o tamanho do arquivo
+    printf("Tamanho do arquivo: %d (Bytes)\n", fSize);
+    printf("Numero de registros: %d\n\n", (fSize/sizeof(Product))); // Divide o tamanho do arquivo pelo tamanho dos elementos, trazendo a quantidade de registros
+    fseek(arqIn, 0l, SEEK_SET);
+
+
+    while(fread(&productIn, sizeof(productIn), 1, arqIn))
+    {
+        printf("%i\t%8s\t%5.1f\n", productIn.id, productIn.name, productIn.price);
+    }
+
+
 }
